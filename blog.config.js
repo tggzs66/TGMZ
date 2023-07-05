31a958232453b5f714f4ebd658069862cbe7f01d
tggzs66 <111702046+tggzs66@users.noreply.github.com>
2023-07-06 02:06:03 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,39 +1,39 @@
-// 注: process.env.XX是Vercel的环境变量，配置方式见：https://docs.tangly1024.com/zh/features/personality
-const BLOG = {
-  // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
-  NOTION_PAGE_ID:
-      process.env.NOTION_PAGE_ID || '02ab3b8678004aa69e9e415905ef32a5',
-  PSEUDO_STATIC: false, // 伪静态路径，开启后所有文章URL都以 .html 结尾。
-  NEXT_REVALIDATE_SECOND: process.env.NEXT_PUBLIC_REVALIDATE_SECOND || 5, // 更新内容缓存间隔 单位(秒)；即每个页面有5秒的纯静态期、此期间无论多少次访问都不会抓取notion数据；调大该值有助于节省Vercel资源、同时提升访问速率，但也会使文章更新有延迟。
-  THEME: process.env.NEXT_PUBLIC_THEME || 'hexo', // 主题， 支持 ['next','hexo',"fukasawa','medium','example','matery','gitbook','simple'] @see https://preview.tangly1024.com
-  THEME_SWITCH: process.env.NEXT_PUBLIC_THEME_SWITCH || false, // 是否显示切换主题按钮
-  LANG: process.env.NEXT_PUBLIC_LANG || 'zh-CN', // e.g 'zh-CN','en-US'  see /lib/lang.js for more.
-  SINCE: 2021, // e.g if leave this empty, current year will be used.
-  APPEARANCE: process.env.NEXT_PUBLIC_APPEARANCE || 'light', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
-  APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
+// 注:process.env.XX是韦尔塞尔的环境变量,配置方式见:https://docs . tangly 1024 . com/zh/features/personality
+常数 博客 = {
+  //重要page_id！！！来自https://www . organization . so/tanghh/02 ab 3b 8678004 aa 69 e 9 e 415905 ef 32 a 5的重复模板
+  概念_页面_ID:
+过程。包封/包围（动词envelop的简写）.概念_页面_ID || 02ab 3b 8678004 aa 69e 9e 415905 ef 32 a 5 ',
+  伪静态: 错误的, // 伪静态路径,开启后所有文章统一资源定位器都以。超文本标记语言结尾。
+  下一个_重新验证_秒:流程。包封/包围（动词envelop的简写）.NEXT_PUBLIC_REVALIDATE_SECOND || 5, // 更新内容缓存间隔 单位（秒);即每个页面有5秒的纯静态期、此期间无论多少次访问都不会抓取概念数据；调大该值有助于节省韦尔塞尔资源、同时提升访问速率,但也会使文章更新有延迟。
+  主题:流程。包封/包围（动词envelop的简写）.下一个_公共_主题 || 赫克索, // 主题,支持['next '，' hexo '，' fukasawa '，' medium '，' example '，' matery '，' gitbook '，' simple ']@参见https://preview.tangly1024.com
+  主题_切换:流程。包封/包围（动词envelop的简写）.下一个_公共_主题_开关 || 错误的, // 是否显示切换主题按钮
+  语言:流程。包封/包围（动词envelop的简写）.NEXT_PUBLIC_LANG || zh-CN ', //例如' zh-CN '，' en-US '有关更多信息，请参见/lib/lang.js。
+  因为: 2021, //例如，如果留空，将使用当前年份。
+  出现:流程。包封/包围（动词envelop的简写）.下一次公开露面 || “光”, // ['亮'，'暗'，'自动']，//亮日间模式，黑暗夜间模式，自动根据时间和主题自动夜间模式
+  外观_黑暗_时间:流程。包封/包围（动词envelop的简写）.NEXT _ PUBLIC _ APPEARANCE _ DARK _ TIME || [18, 6], // 夜间模式起至时间，假的时关闭根据时间自动切换夜间模式
 
-  // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
-  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || 'Hi，我是一个程序员, Hi，我是一个打工人,Hi，我是一个干饭人,欢迎来到我的博客🎉',
+  // 3.14.1版本后,欢迎语在此配置,英文逗号隔开,即可支持多个欢迎语打字效果。
+  问候语_单词:流程。包封/包围（动词envelop的简写）.NEXT _ PUBLIC _问候语_单词 || 嗨，我是一个程序员，嗨，我是一个打工人，嗨，我是一个干饭人,欢迎来到我的博客🎉',
 
-  CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
+  自定义菜单:流程。包封/包围（动词envelop的简写）.下一个_公共_自定义_菜单 || 错误的, // 支持菜单类型,从3.12.0版本起,各主题将逐步支持灵活的二级菜单配置,替代了原来的页类型,此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'NotionNext', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的干饭人🍚', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
-  // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
-  CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
-  CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
-  CONTACT_TWITTER: process.env.NEXT_PUBLIC_CONTACT_TWITTER || '', // 你的twitter个人主页
-  CONTACT_GITHUB: process.env.NEXT_PUBLIC_CONTACT_GITHUB || '', // 你的github个人主页 例如 https://github.com/tangly1024
-  CONTACT_TELEGRAM: process.env.NEXT_PUBLIC_CONTACT_TELEGRAM || '', // 你的telegram 地址 例如 https://t.me/tangly_1024
-  CONTACT_LINKEDIN: process.env.NEXT_PUBLIC_CONTACT_LINKEDIN || '', // 你的linkedIn 首页
-  CONTACT_INSTAGRAM: process.env.NEXT_PUBLIC_CONTACT_INSTAGRAM || '', // 您的instagram地址
+  作者:流程。包封/包围（动词envelop的简写）.下一个_公共_作者 || '沐子科技', // 您的昵称 例如tangly1024
+  个人简历:流程。包封/包围（动词envelop的简写）.NEXT_PUBLIC_BIO || 嗨，我是一个程序员,欢迎来到我的博客！🎉', // 作者简介
+  环:流程。包封/包围（动词envelop的简写）.下一个_公共_链接 || ' http://muzi.sj3r.com/ ', // 网站地址
+  关键词:流程。包封/包围（动词envelop的简写）.下一个公共关键字 || 观念，博客', // 网站关键词 英文逗号隔开
+  // 社交链接,不需要可留空白,例如联系人_微博:“”
+  联系人_电子邮件:流程。包封/包围（动词envelop的简写）.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
+  联系方式_微博:流程。包封/包围（动词envelop的简写）.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
+  联系人_TWITTER:流程。包封/包围（动词envelop的简写）.NEXT_PUBLIC_CONTACT_TWITTER || '', // 你的推特个人主页
+  联系人_GITHUB:流程。包封/包围（动词envelop的简写）.NEXT_PUBLIC_CONTACT_GITHUB || '', // 你的开源代码库个人主页 例如https://github.com/tangly1024
+  联系人_电报:流程。包封/包围（动词envelop的简写）.NEXT_PUBLIC_CONTACT_TELEGRAM || '', // 你的电报地址 例如https://t.me/tangly_1024
+  联系人_LINKEDIN:流程。包封/包围（动词envelop的简写）.NEXT_PUBLIC_CONTACT_LINKEDIN || '', // 你的商务化人际关系网首页
+  联系人_INSTAGRAM:流程。包封/包围（动词envelop的简写）.NEXT_PUBLIC_CONTACT_INSTAGRAM || '', // 您的instagram地址
 
-  NOTION_HOST: process.env.NEXT_PUBLIC_NOTION_HOST || 'https://www.notion.so', // Notion域名，您可以选择用自己的域名进行反向代理，如果不懂得什么是反向代理，请勿修改此项
+  观念_主持人:流程。包封/包围（动词envelop的简写）.下一个公共概念主机 || ' https://www . opinion . so ', //概念域名,您可以选择用自己的域名进行反向代理,如果不懂得什么是反向代理,请勿修改此项
 
   // 网站字体
-  FONT_STYLE: process.env.NEXT_PUBLIC_FONT_STYLE || 'font-serif', // ['font-serif','font-sans'] 两种可选，分别是衬线和无衬线: 参考 https://www.jianshu.com/p/55e410bd2115
+  字体样式:流程。包封/包围（动词envelop的简写）.NEXT_PUBLIC_FONT_STYLE || '字体-衬线', // ['font-serif '，' font-sans']两种可选,分别是衬线和无衬线：参考https://www.jianshu.com/p/55e410bd2115
   FONT_URL: [
     // 字体CSS 例如 https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css
     'https://fonts.googleapis.com/css?family=Bitter&display=swap',
@@ -246,100 +246,100 @@ const BLOG = {
 
   COMMENT_VALINE_CDN: process.env.NEXT_PUBLIC_VALINE_CDN || 'https://unpkg.com/valine@1.5.1/dist/Valine.min.js',
   COMMENT_VALINE_APP_ID: process.env.NEXT_PUBLIC_VALINE_ID || '', // Valine @see https://valine.js.org/quickstart.html 或 https://github.com/stonehank/react-valine#%E8%8E%B7%E5%8F%96app-id-%E5%92%8C-app-key
-  COMMENT_VALINE_APP_KEY: process.env.NEXT_PUBLIC_VALINE_KEY || '',
-  COMMENT_VALINE_SERVER_URLS: process.env.NEXT_PUBLIC_VALINE_SERVER_URLS || '', // 该配置适用于国内自定义域名用户, 海外版本会自动检测(无需手动填写) @see https://valine.js.org/configuration.html#serverURLs
-  COMMENT_VALINE_PLACEHOLDER:
-      process.env.NEXT_PUBLIC_VALINE_PLACEHOLDER || '抢个沙发吧~', // 可以搭配后台管理评论 https://github.com/DesertsP/Valine-Admin  便于查看评论，以及邮件通知，垃圾评论过滤等功能
+COMMENT_VALINE_APP_KEY:过程。包封/包围（动词envelop的简写）.下一个_公共_缬氨酸_密钥 || '',
+COMMENT _ VALINE _ SERVER _ URLS:process。包封/包围（动词envelop的简写）.下一个公共缬氨酸服务器网址 || '', // 该配置适用于国内自定义域名用户,海外版本会自动检测（无需手动填写)@见https://valine.js.org/configuration.html#serverURLs
+COMMENT_VALINE_PLACEHOLDER:
+过程。包封/包围（动词envelop的简写）.NEXT_PUBLIC_VALINE_PLACEHOLDER || '抢个沙发吧~', // 可以搭配后台管理评论https://github.com/DesertsP/Valine-Admin便于查看评论,以及邮件通知,垃圾评论过滤等功能
 
-  COMMENT_WALINE_SERVER_URL: process.env.NEXT_PUBLIC_WALINE_SERVER_URL || '', // 请配置完整的Waline评论地址 例如 hhttps://preview-waline.tangly1024.com @see https://waline.js.org/guide/get-started.html
-  COMMENT_WALINE_RECENT: process.env.NEXT_PUBLIC_WALINE_RECENT || false, // 最新评论
+COMMENT _ WALINE _ SERVER _ URL:process。包封/包围（动词envelop的简写）.下一个公共网站服务器网址 || '', // 请配置完整的沃林评论地址 例如hhttps://preview-waline . tangly 1024 . com @看https://waline.js.org/guide/get-started.html
+COMMENT_WALINE_RECENT:过程。包封/包围（动词envelop的简写）.下一个公共路线最近 || 错误的, // 最新评论
 
-  // 此评论系统基于WebMention，细节可参考https://webmention.io
-  // 它是一个基于IndieWeb理念的开放式评论系统，下方COMMENT_WEBMENTION包含的属性皆需配置：
-  // ENABLE: 是否开启
-  // AUTH: Webmention使用的IndieLogin，可使用Twitter或Github个人页面连结
-  // HOSTNAME: Webmention绑定之网域，通常即为本站网址
-  // TWITTER_USERNAME: 评论显示区域需要的资讯
-  // TOKEN: Webmention的API token
-  COMMENT_WEBMENTION: {
-    ENABLE: process.env.NEXT_PUBLIC_WEBMENTION_ENABLE || false,
-    AUTH: process.env.NEXT_PUBLIC_WEBMENTION_AUTH || '',
-    HOSTNAME: process.env.NEXT_PUBLIC_WEBMENTION_HOSTNAME || '',
-    TWITTER_USERNAME: process.env.NEXT_PUBLIC_TWITTER_USERNAME || '',
-    TOKEN: process.env.NEXT_PUBLIC_WEBMENTION_TOKEN || ''
+  // 此评论系统基于网络提及，细节可参考https://web reference . io
+  // 它是一个基于IndieWeb理念的开放式评论系统,下方评论_网络提及包含的属性皆需配置：
+  //启用:是否开启
+  //AUTH:web reference使用的靛玉红，可使用推特或开源代码库个人页面连结
+  //主机名:web提及绑定之网域,通常即为本站网址
+  //TWITTER _用户名:评论显示区域需要的资讯
+  //TOKEN:web reference的API令牌
+评论_网络提及:{
+启用:流程。包封/包围（动词envelop的简写）.下一个_公共_网站提及_启用 || 错误的,
+授权:流程。包封/包围（动词envelop的简写）.下一个_公共_网络提及_认证 || '',
+主机名:进程。包封/包围（动词envelop的简写）.下一个公共网站名称主机名 || '',
+TWITTER_USERNAME:进程。包封/包围（动词envelop的简写）.NEXT _ PUBLIC _ TWITTER _用户名 || '',
+令牌:进程。包封/包围（动词envelop的简写）.下一个_公共_网络提及_令牌 || ''
   },
 
-  // <---- 评论插件
+  // < - 评论插件
 
-  // ----> 站点统计
-  ANALYTICS_VERCEL: process.env.NEXT_PUBLIC_ANALYTICS_VERCEL || false, // vercel自带的统计 https://vercel.com/docs/concepts/analytics/quickstart https://github.com/tangly1024/NotionNext/issues/897
-  ANALYTICS_BUSUANZI_ENABLE: true, // 展示网站阅读量、访问数 see http://busuanzi.ibruce.info/
-  ANALYTICS_BAIDU_ID: process.env.NEXT_PUBLIC_ANALYTICS_BAIDU_ID || '', // e.g 只需要填写百度统计的id，[baidu_id] -> https://hm.baidu.com/hm.js?[baidu_id]
-  ANALYTICS_CNZZ_ID: process.env.NEXT_PUBLIC_ANALYTICS_CNZZ_ID || '', // 只需要填写站长统计的id, [cnzz_id] -> https://s9.cnzz.com/z_stat.php?id=[cnzz_id]&web_id=[cnzz_id]
-  ANALYTICS_GOOGLE_ID: process.env.NEXT_PUBLIC_ANALYTICS_GOOGLE_ID || '', // 谷歌Analytics的id e.g: G-XXXXXXXXXX
+  // - > 站点统计
+ANALYTICS_VERCEL:过程.包封/包围（动词包围的简写).NEXT_PUBLIC_ANALYTICS_VERCEL|| 错误的,// vercel自带的统计https://vercel.com/docs/concepts/analytics/quickstart·https://github.com/tangly1024/NotionNext/issues/897
+分析_ BUSUANZI _启用：真实的,// 展示网站阅读量、访问数参见http://busuanzi.ibruce.info/
+分析_百度_ID:流程。包封/包围（动词包围的简写).然后_公共_分析_百度_ID ||'', //例如只需要填写百度统计的id，[百度_ id]-> https://hm.baidu.com/hm.js?[百度_id]
+分析_CNZZ_ID:流程.包封/包围（动词包围的简写).NEXT_PUBLIC_ANALYTICS_CNZZ_ID || '', // 只需要填写站长统计的id，[cnzz _ id]-> https://s9.cnzz.com/z_stat.php?id=[cnzz_id]&web_id=[cnzz_id]
+ANALYTICS_GOOGLE_ID: process .包封/包围（动词包围的简写).然后_ PUBLIC _ ANALYTICS _ GOOGLE _ ID ||'', // 谷歌分析学的例如:G-XXXXXXXXXX
 
-  ANALYTICS_ACKEE_TRACKER:
-      process.env.NEXT_PUBLIC_ANALYTICS_ACKEE_TRACKER || '', // e.g 'https://ackee.tangly1024.net/tracker.js'
-  ANALYTICS_ACKEE_DATA_SERVER:
-      process.env.NEXT_PUBLIC_ANALYTICS_ACKEE_DATA_SERVER || '', // e.g https://ackee.tangly1024.net , don't end with a slash
-  ANALYTICS_ACKEE_DOMAIN_ID:
-      process.env.NEXT_PUBLIC_ANALYTICS_ACKEE_DOMAIN_ID || '', // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
+分析_ACKEE_TRACKER:
+过程。包封/包围（动词包围的简写).然后_ PUBLIC _ ANALYTICS _ ACKEE _ TRACKER | |'', //例如" https://ackee。tangly 1024。网/追踪器。js "
+分析_ ACKEE _数据_服务器：
+过程。包封/包围（动词包围的简写).下一个_公共_分析_阿克里数据_服务器 ||'', //例如https://ackee.tangly1024.net，不要以斜线结尾
+分析_ACKEE_DOMAIN_ID:
+过程。包封/包围（动词包围的简写).然后_ PUBLIC _ ANALYTICS _ ACKEE _ DOMAIN _ ID | |'', //例如" 0e 2257 A8-54d 4-4847-91a 1-0311 ea 48 cc 7b "
 
-  SEO_GOOGLE_SITE_VERIFICATION:
-      process.env.NEXT_PUBLIC_SEO_GOOGLE_SITE_VERIFICATION || '', // Remove the value or replace it with your own google site verification code
+SEO_GOOGLE_SITE_VERIFICATION:
+过程。包封/包围（动词包围的简写).然后_ PUBLIC _ SEO _ GOOGLE _ SITE _ VERIFICATION | |'', //删除该值或用您自己的谷歌站点验证码替换它
 
-  // <---- 站点统计
+  // < - 站点统计
 
   // 谷歌广告
-  ADSENSE_GOOGLE_ID: process.env.NEXT_PUBLIC_ADSENSE_GOOGLE_ID || '', // 谷歌广告ID e.g ca-pub-xxxxxxxxxxxxxxxx
-  ADSENSE_GOOGLE_TEST: process.env.NEXT_PUBLIC_ADSENSE_GOOGLE_TEST || false, // 谷歌广告ID测试模式，这种模式获取假的测试广告，用于开发 https://www.tangly1024.com/article/local-dev-google-adsense
+ADSENSE_GOOGLE_ID: process .包封/包围（动词包围的简写).NEXT_PUBLIC_ADSENSE_GOOGLE_ID || '', // 谷歌广告例如ca-pub-xxxxxxxxxxxxxxxxxxxxxx
+ADSENSE_GOOGLE_TEST:过程.包封/包围（动词包围的简写).然后_ PUBLIC _ ADSENSE _ GOOGLE _ TEST ||错误的,// 谷歌广告身份证明测试模式,这种模式获取假的测试广告,用于开发https://www.tangly1024.com/article/local-dev-google-adsense
 
-  // 自定义配置notion数据库字段名
-  NOTION_PROPERTY_NAME: {
-    password: process.env.NEXT_PUBLIC_NOTION_PROPERTY_PASSWORD || 'password',
-    type: process.env.NEXT_PUBLIC_NOTION_PROPERTY_TYPE || 'type', // 文章类型，
-    type_post: process.env.NEXT_PUBLIC_NOTION_PROPERTY_TYPE_POST || 'Post', // 当type文章类型与此值相同时，为博文。
-    type_page: process.env.NEXT_PUBLIC_NOTION_PROPERTY_TYPE_PAGE || 'Page', // 当type文章类型与此值相同时，为单页。
-    type_notice:
-        process.env.NEXT_PUBLIC_NOTION_PROPERTY_TYPE_NOTICE || 'Notice', // 当type文章类型与此值相同时，为公告。
-    type_menu: process.env.NEXT_PUBLIC_NOTION_PROPERTY_TYPE_MENU || 'Menu', // 当type文章类型与此值相同时，为菜单。
-    type_sub_menu:
-        process.env.NEXT_PUBLIC_NOTION_PROPERTY_TYPE_SUB_MENU || 'SubMenu', // 当type文章类型与此值相同时，为子菜单。
-    title: process.env.NEXT_PUBLIC_NOTION_PROPERTY_TITLE || 'title', // 文章标题
-    status: process.env.NEXT_PUBLIC_NOTION_PROPERTY_STATUS || 'status',
-    status_publish:
-        process.env.NEXT_PUBLIC_NOTION_PROPERTY_STATUS_PUBLISH || 'Published', // 当status状态值与此相同时为发布，可以为中文
-    status_invisible:
-        process.env.NEXT_PUBLIC_NOTION_PROPERTY_STATUS_INVISIBLE || 'Invisible', // 当status状态值与此相同时为隐藏发布，可以为中文 ， 除此之外其他页面状态不会显示在博客上
-    summary: process.env.NEXT_PUBLIC_NOTION_PROPERTY_SUMMARY || 'summary',
-    slug: process.env.NEXT_PUBLIC_NOTION_PROPERTY_SLUG || 'slug',
-    category: process.env.NEXT_PUBLIC_NOTION_PROPERTY_CATEGORY || 'category',
-    date: process.env.NEXT_PUBLIC_NOTION_PROPERTY_DATE || 'date',
-    tags: process.env.NEXT_PUBLIC_NOTION_PROPERTY_TAGS || 'tags',
-    icon: process.env.NEXT_PUBLIC_NOTION_PROPERTY_ICON || 'icon'
+  // 自定义配置概念数据库字段名
+概念_属性_名称：{
+密码：流程。包封/包围（动词包围的简写).下一个公共概念属性密码 || '密码',
+类型：流程。包封/包围（动词包围的简写).下一个公共概念属性类型 || '类型', // 文章类型,
+type_post:流程。包封/包围（动词包围的简写).然后_ PUBLIC _ idea _ PROPERTY _ TYPE _ POST | |'发布', // 当类型文章类型与此值相同时,为博文。
+类型_页面：流程。包封/包围（动词包围的简写).下一个公共概念属性类型页面 || '页面', // 当类型文章类型与此值相同时,为单页。
+类型_通知：
+过程。包封/包围（动词包围的简写).下一个公共概念属性类型通知 || '注意', // 当类型文章类型与此值相同时,为公告。
+类型_菜单：流程。包封/包围（动词包围的简写).下一个公共概念属性类型菜单 || '菜单', // 当类型文章类型与此值相同时,为菜单。
+类型_子_菜单：
+过程。包封/包围（动词包围的简写).下一个公共概念属性类型子菜单 || '子菜单', // 当类型文章类型与此值相同时,为子菜单。
+标题：流程。包封/包围（动词包围的简写).下一个_公共_概念_属性_标题 || '标题', // 文章标题
+状态：流程。包封/包围（动词包围的简写).下一个公共概念属性状态 || '状态',
+状态_发布：
+过程。包封/包围（动词包围的简写).下一个_公共_概念_属性_状态_发布 || '已发布', // 当状态状态值与此相同时为发布,可以为中文
+状态_不可见：
+过程。包封/包围（动词包围的简写).下一个_公共_概念_属性_状态_不可见 || '隐形', // 当状态状态值与此相同时为隐藏发布,可以为中文,除此之外其他页面状态不会显示在博客上
+摘要：流程。包封/包围（动词包围的简写).下一个_公共_概念_属性_摘要 || '摘要',
+鼻涕虫：流程。包封/包围（动词包围的简写).然后_ PUBLIC _ idea _ PROPERTY _ SLUG ||鼻涕虫,
+种类：流程。包封/包围（动词包围的简写).下一个_公共_概念_属性_类别 || '类别',
+日期：流程。包封/包围（动词包围的简写).下一个公共概念属性日期|| 日期,
+标签：流程。包封/包围（动词包围的简写).下一个公共概念属性标签 || '标签',
+图标：流程。包封/包围（动词包围的简写).下一个公共概念属性图标 || '图标'
   },
 
   // RSS
-  ENABLE_RSS: process.env.NEXT_PUBLIC_ENABLE_RSS || true, // 是否开启RSS订阅功能
+启用_RSS:进程。包封/包围（动词包围的简写).NEXT_PUBLIC_ENABLE_RSS|| 真实的,// 是否开启简易资讯聚合订阅功能
 
   // 作废配置
-  AVATAR: process.env.NEXT_PUBLIC_AVATAR || '/avatar.svg', // 作者头像，被notion中的ICON覆盖。若无ICON则取public目录下的avatar.png
-  TITLE: process.env.NEXT_PUBLIC_TITLE || 'NotionNext BLOG', // 站点标题 ，被notion中的页面标题覆盖；此处请勿留空白，否则服务器无法编译
-  HOME_BANNER_IMAGE:
-      process.env.NEXT_PUBLIC_HOME_BANNER_IMAGE || './bg_image.jpg', // 首页背景大图, 会被notion中的封面图覆盖，若无封面图则会使用代码中的 /public/bg_image.jpg 文件
-  DESCRIPTION:
-      process.env.NEXT_PUBLIC_DESCRIPTION || '这是一个由NotionNext生成的站点', // 站点描述，被notion中的页面描述覆盖
+头像：过程。包封/包围（动词包围的简写).NEXT_PUBLIC_AVATAR || /avatar.svg '，// 作者头像,被概念中的图标覆盖。若无图标则取公众的目录下的阿凡达。png
+标题：过程。包封/包围（动词包围的简写).下一个_公共_标题 || '没有下一个博客', // 站点标题,被概念中的页面标题覆盖；此处请勿留空白,否则服务器无法编译
+主页_横幅_图片：
+过程。包封/包围（动词envelop的简写）.NEXT_PUBLIC_HOME_BANNER_IMAGE || 。/bg_image.jpg ', // 首页背景大图,会被概念中的封面图覆盖,若无封面图则会使用代码中的/public/bg_image.jpg文件
+描述:
+过程。包封/包围（动词envelop的简写）.下一个_公共_描述 || '这是一个由沐子科技制作的站点', // 站点描述,被概念中的页面描述覆盖
 
   // 网站图片
-  IMG_URL_TYPE: process.env.NEXT_PUBLIC_IMG_TYPE || 'Notion', // 此配置已失效，请勿使用；AMAZON方案不再支持，仅支持Notion方案。 ['Notion','AMAZON'] 站点图片前缀 默认 Notion:(https://notion.so/images/xx) ， AMAZON(https://s3.us-west-2.amazonaws.com/xxx)
-  IMG_SHADOW: process.env.NEXT_PUBLIC_IMG_SHADOW || false, // 文章图片是否自动添加阴影
+IMG URL类型:流程。包封/包围（动词envelop的简写）.NEXT_PUBLIC_IMG_TYPE || “观念”, // 此配置已失效,请勿使用；亚马逊河方案不再支持,仅支持概念方案. . .['观念'，'亚马逊']站点图片前缀 默认观念:(https://opinion . so/images/xx)、亚马逊(https://S3 . us-west-2 . AMAZON AWS . com/XXX)
+IMG _影子:过程。包封/包围（动词envelop的简写）.下一个_公共_ IMG _影子 || 错误的, // 文章图片是否自动添加阴影
 
   // 开发相关
-  NOTION_ACCESS_TOKEN: process.env.NOTION_ACCESS_TOKEN || '', // Useful if you prefer not to make your database public
-  DEBUG: process.env.NEXT_PUBLIC_DEBUG || false, // 是否显示调试按钮
-  ENABLE_CACHE: process.env.ENABLE_CACHE || false, // 开启缓存会将Notion数据缓存在内存中，通常在开发调试中使用，正式部署开启此功能意义不大。
-  isProd: process.env.VERCEL_ENV === 'production', // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)  isProd: process.env.VERCEL_ENV === 'production' // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
-  VERSION: process.env.NEXT_PUBLIC_VERSION // 版本号
+概念访问令牌:进程。包封/包围（动词envelop的简写）.概念_访问_令牌 || '', //如果您不想公开您的数据库，这很有用
+调试:进程。包封/包围（动词envelop的简写）.NEXT_PUBLIC_DEBUG || 错误的, // 是否显示调试按钮
+ENABLE_CACHE: process。包封/包围（动词envelop的简写）.启用_缓存 || 错误的, // 开启缓存会将概念数据缓存在内存中,通常在开发调试中使用,正式部署开启此功能意义不大。
+伊斯普罗德:过程。包封/包围（动词envelop的简写）.韦尔塞尔_环境 === “生产”, //区分开发和生产环境(ref:https://vercel . com/docs/environment-variables # system-environment-variables)is prod:process . ENV . vercel _ ENV = = = ' production '//区分开发和生产环境(ref:https://vercel . com/docs/environment-variables # system-environment-variables)
+版本:流程。包封/包围（动词envelop的简写）.下一个公共版本 // 版本号
 }
 
-module.exports = BLOG
+模块。出口=博客
